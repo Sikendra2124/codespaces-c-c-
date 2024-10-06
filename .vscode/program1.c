eb1b29e6815f246f7b5a8cb484322a5d118786a5
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2024-10-06 06:29:02 +0000

    program1 added

---
+++ .vscode/program1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the no. for entire week days");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+        printf("sunday");
+        break;
+        case 2:
+        printf("monday");
+        break;
+        case 3:
+        printf("tuesday");
+        break;
+        case 4:
+        printf("wednesday");
+        break;
+        case 5:
+        printf("thursday");
+        break;
+        case 6:
+        printf("friday");
+        break;
+        case 7:
+        printf("saturday");
+        break;
+        default:
+        printf("wrong choice");
+    }
+    return 0;
+}
