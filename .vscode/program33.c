2a7d7974bc24cd7ca1a9905a934a9cdca740e736
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2024-10-08 09:55:51 +0000

    program added

---
+++ .vscode/program33.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("sum of series=%d",sum);
+}
