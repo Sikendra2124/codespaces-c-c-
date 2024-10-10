ac3144aa5a6e30c58a56ef144535d6707ac0f89c
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2024-10-10 14:52:47 +0000

    program added

--- .vscode/program35.c
+++ .vscode/program35.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,sum=0,t;
-    const int x=9;
+    int i,n,sum=0,t=9;
     printf("enter the number of terms");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        t=x*10+9;
         sum=sum+t;
+        // printf(t);
+        t=t*10+9;
     }
     printf("sum of series=%d",sum);
+    return 0;
 }
