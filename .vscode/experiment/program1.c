e097248d1695cd0311c58e5c5fb82c398023234a
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2024-11-04 17:29:42 +0000

    added

---
+++ .vscode/experiment/program1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum;
+    printf("input num1 and num2\n");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("Sum of numbers=%d",sum);
+    return 0;
+}
