84ec6c3b5a43497240162d8c7499de71b3a7f716
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2024-12-03 11:51:06 +0000

    star pattern successfully added

--- Technical Training/hollow_right_angle_star_pattern.c
+++ Technical Training/hollow_right_angle_star_pattern.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+int main(){
+    int n;
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(j==1||j==i||i==n){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
