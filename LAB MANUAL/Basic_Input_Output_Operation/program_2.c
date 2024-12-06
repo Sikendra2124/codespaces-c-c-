c88d50fb11f03cdef8bd28723a53d76b151c5c60
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2024-12-06 13:59:59 +0000

    program added

---
+++ LAB MANUAL/Basic_Input_Output_Operation/program_2.c
@@ -0,0 +1,22 @@
+// #include<stdio.h>
+// int main(){
+//     float celsius,fahrenheit;
+//     printf("***celcious to Fahrenheit***\n");
+//     printf("enter celcious:\t");
+//     scanf("%f",&celsius);
+//     fahrenheit=(celsius*9/5)+32;
+//     printf("converted celcius=%f",fahrenheit);
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main(){
+    float celsius,fahrenheit;
+    printf("***fahrenhiet to celcius***\n");
+    printf("enter fahrenhiet:\t");
+    scanf("%f",&fahrenheit);
+    celsius=(fahrenheit*9/5)+32;
+    printf("converted fahrenhiet=%f",celsius);
+    return 0;
+}
